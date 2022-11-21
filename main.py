9c4c3b0e86ccdaac0f66b9992ceb07cbde1f372c
Vladimir Gorbunov <110591063+VladimirGorF@users.noreply.github.com>
2022-11-21 18:50:48 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#__________________________BAR_Progress________________________________________________
+# from progress.bar import Bar
+# import time
+#
+# bar = Bar('Processing', max=20)
+# for i in range(20):
+#     time.sleep(0.3)
+#     print('*', end="")
+#     bar.next()
+# bar.finish()
+#
+# #______________________ГРАФИКИ_________________________________________________________
+# import matplotlib.pyplot as plt
+# import numpy as np
+# list = [1,5,8,2,9,5]
+# plt.plot(list)
+# plt.show()
